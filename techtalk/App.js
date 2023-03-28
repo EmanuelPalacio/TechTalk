a44ed555ee2e0b05a8ccbb78d7a15209e7c19a50
FranciscoSantos01 <franciscosantosaraoz2000@gmail.com>
2023-03-28 18:50:44 -0300

    Avanzes login y registro

--- techtalk/App.js
+++ techtalk/App.js
@@ -5,18 +5,18 @@ import Navigation from './src/navigation';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.root}>
+    <>
       <Navigation />
-    </SafeAreaView>
+    </>
   );
 }
 // <StatusBar style="auto" />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
--- techtalk/src/navigation/index.js
+++ techtalk/src/navigation/index.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignInScreen from '../screens/SignInScreen/SignInScreen';
-import SignUpScreen from '../screens/SignUpScreen/SignUpScreen';
+
 import HomeScreen from '../screens/HomeScreen'
+import { AuthNavigation } from '../../auth/navigation/AuthNavigation';
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Navigator initialRouteName='Auth' screenOptions={{headerShown: false}}>
             <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="SignIn" component={SignInScreen} />
-            <Stack.Screen name="SignUp" component={SignUpScreen} />
+            <Stack.Screen name="Auth" component={AuthNavigation} />
         </Stack.Navigator>
       
     </NavigationContainer>
