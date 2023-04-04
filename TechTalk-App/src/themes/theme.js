eed098fc919876f7664a169517071a79eebf7ed5
Emanuel Palacio <emanuelpalacio.dev@gmail.com>
2023-04-04 00:05:06 -0300

    init app proyect

---
+++ TechTalk-App/src/themes/theme.js
@@ -0,0 +1,22 @@
+const theme = {
+  colors: {
+    primary: '#FFFFFF',
+    secondary: '#2D4C7E',
+    secondaryTransparency: '#2D4C7E33',
+    third: '#5FBBE8',
+    PrimaryText: '#000000',
+    secondText: '#FFFFFF',
+    thirdText: '#3154A5',
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16,
+    header: 20,
+  },
+  fontWeights: {
+    normal: 400,
+  },
+  fonts: {
+    main: 'Geomanist, sans-serif',
+  },
+};
