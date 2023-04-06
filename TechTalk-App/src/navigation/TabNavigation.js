189e99d85562b9ddbb953e4c046ff0efb9599197
Emanuel Palacio <emanuelpalacio.dev@gmail.com>
2023-04-06 09:31:39 -0300

    TabNavigator

--- TechTalk-App/src/navigation/TabNavigation.js
+++ TechTalk-App/src/navigation/TabNavigation.js
@@ -1,17 +1,101 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import LoadScreen from '../screens/LoadScreen.js';
 import ComingSoonScreen from '../screens/ComingSoonScreen.js';
+import { Image } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name='Conectar' component={ComingSoonScreen} />
-      <Tab.Screen name='Chat' component={ComingSoonScreen} />
-      <Tab.Screen name='Aprende' component={ComingSoonScreen} />
-      <Tab.Screen name='Perfil' component={ComingSoonScreen} />
+      <Tab.Screen
+        name='Conectar'
+        component={ComingSoonScreen}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Image
+                source={
+                  focused
+                    ? {
+                        uri: 'https://res.cloudinary.com/dshfifpgv/image/upload/v1680781051/Images%20proyect%20techTalk/TechTalkAssets/TabIcons/ConectarSelected_xb0jdv.png',
+                      }
+                    : {
+                        uri: 'https://res.cloudinary.com/dshfifpgv/image/upload/v1680780236/Images%20proyect%20techTalk/TechTalkAssets/TabIcons/Conectar_iw8ylh.png',
+                      }
+                }
+                style={{ width: 20, height: 20 }}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name='Chat'
+        component={ComingSoonScreen}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Image
+                source={
+                  focused
+                    ? {
+                        uri: 'https://res.cloudinary.com/dshfifpgv/image/upload/v1680780236/Images%20proyect%20techTalk/TechTalkAssets/TabIcons/ChatSelected_loyp6c.png',
+                      }
+                    : {
+                        uri: 'https://res.cloudinary.com/dshfifpgv/image/upload/v1680780236/Images%20proyect%20techTalk/TechTalkAssets/TabIcons/Chat_ekcelp.png',
+                      }
+                }
+                style={{ width: 20, height: 20 }}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name='Aprende'
+        component={ComingSoonScreen}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Image
+                source={
+                  focused
+                    ? {
+                        uri: 'https://res.cloudinary.com/dshfifpgv/image/upload/v1680781051/Images%20proyect%20techTalk/TechTalkAssets/TabIcons/AprenderSelected_oisheu.png',
+                      }
+                    : {
+                        uri: 'https://res.cloudinary.com/dshfifpgv/image/upload/v1680780236/Images%20proyect%20techTalk/TechTalkAssets/TabIcons/Aprender_gd6wxn.png',
+                      }
+                }
+                style={{ width: 20, height: 20 }}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name='Perfil'
+        component={ComingSoonScreen}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Image
+                source={
+                  focused
+                    ? {
+                        uri: 'https://res.cloudinary.com/dshfifpgv/image/upload/v1680781051/Images%20proyect%20techTalk/TechTalkAssets/TabIcons/PerfilSelected_noad8k.png',
+                      }
+                    : {
+                        uri: 'https://res.cloudinary.com/dshfifpgv/image/upload/v1680780236/Images%20proyect%20techTalk/TechTalkAssets/TabIcons/Perfil_zyjklt.png',
+                      }
+                }
+                style={{ width: 20, height: 20 }}
+              />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
--- TechTalk-App/src/screens/ComingSoonScreen.js
+++ TechTalk-App/src/screens/ComingSoonScreen.js
@@ -1,17 +1,21 @@
-import { StyleSheet, View } from 'react-native';
-import { SvgUri } from 'react-native-svg';
+import { Image, StyleSheet, View } from 'react-native';
+import StyledText from '../components/StyledText.js';
 
 export default function ComingSoonScreen() {
   return (
-    <View style={style.screen}>
-      <SvgUri
-        width='100%'
-        uri='https://res.cloudinary.com/dshfifpgv/image/upload/v1680744203/Images%20proyect%20techTalk/TechTalkAssets/ComingSoon_m93epn.svg'
+    <View style={styles.screen}>
+      <StyledText fontSize='title' fontWeight='bold' color='third'>
+        En construcción...
+      </StyledText>
+      <Image
+        source={{
+          uri: 'https://res.cloudinary.com/dshfifpgv/image/upload/v1680781222/Images%20proyect%20techTalk/TechTalkAssets/En_construcci%C3%B3n..._qqi8az.png',
+        }}
       />
     </View>
   );
 }
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   screen: {
     justifyContent: 'center',
     alignItems: 'center',
