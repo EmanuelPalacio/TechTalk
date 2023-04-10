7ea4fb1386eba660370ef6e3264c0969e12be6b0
Emanuel Palacio <emanuelpalacio.dev@gmail.com>
2023-04-10 12:14:02 -0300

    install chart

--- TechTalk-App/src/screens/ProfileScreen.js
+++ TechTalk-App/src/screens/ProfileScreen.js
@@ -8,6 +8,7 @@ import {
 import theme from '../themes/theme.js';
 import StyledText from '../components/StyledText.js';
 import { LinearGradient } from 'expo-linear-gradient';
+import { SvgUri } from 'react-native-svg';
 
 const ProfileScreen = () => {
   return (
@@ -41,7 +42,13 @@ const ProfileScreen = () => {
           end={{ x: 0.7, y: 0 }}
           style={styles.activity}
         >
-          <StyledText>Reading</StyledText>
+          <SvgUri uri='https://res.cloudinary.com/dshfifpgv/image/upload/v1681128139/Images%20proyect%20techTalk/TechTalkAssets/Defaut%20avatar%20profile/Reading_tglith.svg' />
+          <StyledText color='fourth' fontWeight='bold' fontSize='subheading'>
+            Reading
+          </StyledText>
+          <StyledText color='fourth' fontWeight='bold' fontSize='subheading'>
+            90%
+          </StyledText>
         </LinearGradient>
         <LinearGradient
           // Button Linear Gradient
@@ -50,7 +57,13 @@ const ProfileScreen = () => {
           end={{ x: 0.7, y: 0 }}
           style={styles.activity}
         >
-          <StyledText>Reading</StyledText>
+          <SvgUri uri='https://res.cloudinary.com/dshfifpgv/image/upload/v1681128139/Images%20proyect%20techTalk/TechTalkAssets/Defaut%20avatar%20profile/Listening_egryof.svg' />
+          <StyledText color='fourth' fontWeight='bold' fontSize='subheading'>
+            Listening
+          </StyledText>
+          <StyledText color='fourth' fontWeight='bold' fontSize='subheading'>
+            90%
+          </StyledText>
         </LinearGradient>
         <LinearGradient
           // Button Linear Gradient
@@ -59,7 +72,13 @@ const ProfileScreen = () => {
           end={{ x: 0.7, y: 0 }}
           style={styles.activity}
         >
-          <StyledText>Reading</StyledText>
+          <SvgUri uri='https://res.cloudinary.com/dshfifpgv/image/upload/v1681128139/Images%20proyect%20techTalk/TechTalkAssets/Defaut%20avatar%20profile/Writing_cfuxxr.svg' />
+          <StyledText color='fourth' fontWeight='bold' fontSize='subheading'>
+            Writing
+          </StyledText>
+          <StyledText color='fourth' fontWeight='bold' fontSize='subheading'>
+            90%
+          </StyledText>
         </LinearGradient>
         <LinearGradient
           // Button Linear Gradient
@@ -68,7 +87,13 @@ const ProfileScreen = () => {
           end={{ x: 0.7, y: 0 }}
           style={styles.activity}
         >
-          <StyledText>Reading</StyledText>
+          <SvgUri uri='https://res.cloudinary.com/dshfifpgv/image/upload/v1681128139/Images%20proyect%20techTalk/TechTalkAssets/Defaut%20avatar%20profile/Speaking_txag08.svg' />
+          <StyledText color='fourth' fontWeight='bold' fontSize='subheading'>
+            Speaking
+          </StyledText>
+          <StyledText color='fourth' fontWeight='bold' fontSize='subheading'>
+            90%
+          </StyledText>
         </LinearGradient>
       </View>
     </ScrollView>
@@ -119,10 +144,13 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   activity: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
     height: 50,
     width: '90%',
     borderRadius: 10,
-    borderWidth: 3,
+    borderWidth: 2,
     borderColor: theme.colors.secondaryTransparency,
   },
 });
