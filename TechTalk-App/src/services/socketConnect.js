a37830a7b6fbd8454259937d9c27ae607bc04b37
Emanuel Palacio <emanuelpalacio.dev@gmail.com>
2023-04-12 17:22:37 -0300

    config enviroments names

--- TechTalk-App/src/services/socketConnect.js
+++ TechTalk-App/src/services/socketConnect.js
@@ -1,7 +1,7 @@
-import { LOCAL_HOST, API_URL } from '@env';
+import { LOCAL_HOST, HOST_PROD } from '@env';
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'development' ? LOCAL_HOST : API_URL;
+const URL = process.env.NODE_ENV === 'development' ? LOCAL_HOST : HOST_PROD;
 
 export const socket = io(URL, {
   autoConnect: false,
