6e43a8c2826fcc624e966639fb2bd7075837354b
Emanuel Palacio <emanuelpalacio.dev@gmail.com>
2023-04-12 20:44:51 -0300

    fixed input style

--- TechTalk-App/src/components/StyledInput.js
+++ TechTalk-App/src/components/StyledInput.js
@@ -14,14 +14,7 @@ export default function StyledInput({
 }) {
   return (
     <View style={styles.container}>
-      <SvgUri
-        width='50'
-        uri={url}
-        style={{
-          marginLeft: 5,
-          marginRight: 5,
-        }}
-      />
+      <SvgUri width='50' uri={url} />
       <TextInput
         placeholder={placeholder}
         value={value}
@@ -32,17 +25,11 @@ export default function StyledInput({
         style={styles.input}
       />
       <TouchableOpacity
+        style={styles.secondIcon}
         hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
         onPress={() => action()}
       >
-        <SvgUri
-          width='50'
-          uri={secondUrl}
-          style={{
-            marginLeft: 5,
-            marginRight: 5,
-          }}
-        />
+        <SvgUri width='50' uri={secondUrl} />
       </TouchableOpacity>
     </View>
   );
@@ -54,17 +41,22 @@ const styles = StyleSheet.create({
     fontSize: theme.fontSizes.subheading,
     fontFamily: theme.fontFamily.main,
     paddingHorizontal: 5,
-    width: '70%',
+    width: '75%',
     height: '100%',
   },
   container: {
+    position: 'relative',
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     width: '90%',
     height: 50,
     borderRadius: 10,
     borderWidth: 2,
     borderColor: theme.colors.secondaryTransparency,
+  },
+  secondIcon: {
+    position: 'absolute',
+    right: 0,
   },
 });
