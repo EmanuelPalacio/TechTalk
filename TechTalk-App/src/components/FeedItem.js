1f81145499efb233de399a249371c22f731777d9
dev-back55 <ahabitu@gmail.com>
2023-04-14 08:44:05 -0300

    Screens Conectar Feed

---
+++ TechTalk-App/src/components/FeedItem.js
@@ -0,0 +1,99 @@
+import { Image, StyleSheet, TouchableOpacity, View, Text } from 'react-native';
+import StyledText from './StyledText.js';
+import theme from '../themes/theme.js';
+import { useNavigation } from '@react-navigation/native';
+
+const FeedItem = ({conectUser}) => {
+    const navigation = useNavigation();
+    return (
+      <TouchableOpacity
+        style={styles.container}
+        // onPress={() =>
+        //   navigation.navigate('Menssages', {
+        //     idConversation,
+        //     contactName: conectUser.fullname,
+        //   })
+        // }
+      >
+       
+        <Image
+          style={styles.image}
+          source={{
+            uri: conectUser.image.url,
+          }}
+        />
+        <View style={styles.containeruser}>
+            <StyledText fontWeight='bold' fontSize='subheading'>
+            {conectUser.fullname}
+            </StyledText>
+            <StyledText fontWeight='normal' fontSize='body'>
+                <Image style={styles.performance}
+                    source={{
+                        uri: "https://res.cloudinary.com/dshfifpgv/image/upload/v1681407729/Images%20proyect%20techTalk/TechTalkAssets/icons/HighConnection_qwqkem.png"
+                    }}
+                /> {conectUser.performance ? conectUser.performance : '10'}
+            </StyledText>
+        </View>
+        <View style={styles.containericons}>
+            <Image style={styles.flag}
+                source={{ 
+                    uri: (conectUser.country ?  conectUser.country : "https://t4.ftcdn.net/jpg/01/06/07/43/240_F_106074343_rDq3QWNlgK6lQWI5BbgwoY5AM5D3qIDY.jpg")
+
+                }}
+            />
+            <Image style={styles.chat}
+                source={{
+                    uri: "https://res.cloudinary.com/dshfifpgv/image/upload/v1681404800/Images%20proyect%20techTalk/TechTalkAssets/icons/chat-conversation-speak-_ucsjgf.png"
+                }}
+            />
+        </View>  
+
+      </TouchableOpacity>
+    );
+  };
+export default FeedItem;
+
+const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      height: 100,
+      width: '100%',
+      marginVertical: 5,
+      borderColor: theme.colors.secondaryTransparency,
+      borderWidth: 1,
+      borderRadius: 20,
+    },
+    image: {
+      width: 60,
+      height: 60,
+      borderRadius: 50,
+      marginHorizontal: 20,
+    },
+    containeruser: {
+        flexDirection: 'column'
+    },
+    containericons: {
+        flexDirection: 'column',
+        padding: 15,
+        justifyContent: 'space-around',
+    },
+    chat: {
+        width: 40,
+        height: 40,
+        borderRadius: 50,
+        
+    },
+    flag: {
+        width: 30,
+        height: 30,
+        borderRadius: 50,
+        
+    },
+    performance: {
+        width: 15,
+        height: 15,
+        
+    }
+  });
