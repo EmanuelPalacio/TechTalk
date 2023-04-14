6589976c6a900fcc88a12c64bf7baaabfae29d50
Emanuel Palacio <emanuelpalacio.dev@gmail.com>
2023-04-14 13:21:37 -0300

    fix view

--- TechTalk-App/App.js
+++ TechTalk-App/App.js
@@ -3,11 +3,12 @@ import { store } from './src/store/store.js';
 import RootNavigation from './src/navigation/RootNavigation.js';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar hidden={false} style='auto' />
+    <SafeAreaView style={{ flex: 1, marginTop: Constants.statusBarHeight }}>
+      <StatusBar hidden={false} style='dark' />
       <Provider store={store}>
         <RootNavigation />
       </Provider>
